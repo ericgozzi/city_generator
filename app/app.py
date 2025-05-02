b46dd8feaee5f927d6fd7277c485b3522fb08a75
ericgozzi <eric.gozzi.mail@gmail.com>
2025-05-02 15:43:10 +0200

    update app

--- app/app.py
+++ app/app.py
@@ -15,7 +15,7 @@ def get_connections_of_a_question(theka, question):
 
 
     quotebook = Quotebook(answers)
-    text, footnotes, title = quotebook.build_text(create_title=True, n=30)
+    text, footnotes, title = quotebook.build_text(create_title=True, n=deepness)
 
     narrative = filter_by_places(text)
     narrative = clean_whitespaces(narrative)
@@ -63,21 +63,32 @@ def build_the_city(library: str, questions: list[str]):
 
 library = st.selectbox(
     "Library",
-    ("urbotheka", "hitchhikers_guide", "cookbooks"),
+    ("urbotheka", "hitchhikers_guide", "cookbooks")
 )
 
 questions = st.text_input("Concepts", 'house museum apple')
 
 
-questions = questions.lower()
-questions = questions.split(' ')
+
+# BUTTONS LAYOUT
+settings_col, generate_col = st.columns(2)
 
 
-if st.button("Generate"):
+# ADVANCED SETTINGS
+with settings_col.popover('Advanced Settings', use_container_width=True, icon="⚙️"):
+    deepness = st.slider('Deepness', 1, 100, 30, 1)
+    resume = st.checkbox('Show resume')
+
+
+# GENERATE BUTTON
+if generate_col.button('Generate City', use_container_width=True, icon="🌆", type="primary"):
+    questions = questions.lower()
+    questions = questions.split(' ')
     try:
         with st.spinner("HAL is reading the books...", show_time=True):
             image = build_the_city(library, questions)
         st.image(image.np_array)
-        st.text(f'Library: {library} \nTopics: {', '.join(questions)}')
+        if resume:
+            st.text(f'Library: {library} \nConcepts: {', '.join(questions)} \nDeepness: {deepness}')
     except:
         st.error('This is embarassing... try to change the concepts.', icon="😳")
